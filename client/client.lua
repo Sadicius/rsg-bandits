1aecff5e4e35250e79517b8c3f8ce446fe3c4a2a
Sadicius <124639760+Sadicius@users.noreply.github.com>
2023-12-21 13:20:23 +0100

    Update client.lua
    less changes
    
    dist

--- client/client.lua
+++ client/client.lua
@@ -4,26 +4,33 @@ local calloffbandits = false
 local cooldownSecondsRemaining = 0
 local npcs = {}
 local horse = {}
+local isLoggedIn = false
 
-Citizen.CreateThread(function()
-    while true do
-        Wait(1000)
-        for v,k in pairs(Config.Bandits) do
-            local coords = GetEntityCoords(PlayerPedId())
-            local dis = GetDistanceBetweenCoords(coords.x, coords.y, coords.z, k.triggerPoint.x, k.triggerPoint.y, k.triggerPoint.z)
-            if dis < Config.TriggerBandits and spawnbandits == false then
-                --print('trigger bandits')
-                banditsTrigger(k.bandits)
-            end
-            if dis >= Config.CalloffBandits and spawnbandits == true then
-                --print('call off bandits')
-                calloffbandits = true
-            end
-        end
-    end
+----------------------------------------------------------------------------
+-- On Player Loaded
+----------------------------------------------------------------------------
+RegisterNetEvent('RSGCore:Client:OnPlayerLoaded', function()
+    isLoggedIn = true
 end)
 
-function banditsTrigger(bandits)
+----------------------------------------------------------------------------
+-- cooldown timer
+----------------------------------------------------------------------------
+local function cooldownTimer()
+    cooldownSecondsRemaining = Config.Cooldown
+    Citizen.CreateThread(function()
+        while cooldownSecondsRemaining > 0 do
+            Wait(1000)
+            cooldownSecondsRemaining = cooldownSecondsRemaining - 1
+            print(cooldownSecondsRemaining)
+        end
+    end)
+end
+
+----------------------------------------------------------------------------
+-- bandits Trigger
+----------------------------------------------------------------------------
+local function banditsTrigger(bandits)
     spawnbandits = true
     for v,k in pairs(bandits) do
         local horsemodel = GetHashKey(Config.HorseModels[math.random(1,#Config.HorseModels)])
@@ -32,7 +39,7 @@ function banditsTrigger(bandits)
         RequestModel(banditmodel)
         if not HasModelLoaded(banditmodel) then RequestModel(banditmodel) end
         while not HasModelLoaded(banditmodel) do Wait(1) end
-        Citizen.Wait(100)
+        Wait(100)
         -- create bandits
         npcs[v] = CreatePed(banditmodel, k, true, true, true, true)
         Citizen.InvokeNative(0x283978A15512B2FE, npcs[v], true)
@@ -44,7 +51,7 @@ function banditsTrigger(bandits)
         RequestModel(horsemodel)
         if not HasModelLoaded(horsemodel) then RequestModel(horsemodel) end
         while not HasModelLoaded(horsemodel) do Wait(1) end
-        Citizen.Wait(100)
+        Wait(100)
         horse[v] = CreatePed(horsemodel, k, true, true, true, true)
         Citizen.InvokeNative(0x283978A15512B2FE, horse[v], true)
         Citizen.InvokeNative(0xD3A7B003ED343FD9, horse[v],0x20359E53,true,true,true) --saddle
@@ -57,12 +64,34 @@ function banditsTrigger(bandits)
     end
 end
 
-Citizen.CreateThread(function()
+----------------------------------------------------------------------------
+-- Spawn
+----------------------------------------------------------------------------
+CreateThread(function()
+    while true do
+        Wait(1000)
+        for v,k in pairs(Config.Bandits) do
+            local coords = GetEntityCoords(PlayerPedId())
+            local coords3 = vector3(k.triggerPoint.x, k.triggerPoint.y, k.triggerPoint.z)
+            local dis = #(coords - coords3)
+            if dis < Config.TriggerBandits and not spawnbandits then
+                --print('trigger bandits')
+                banditsTrigger(k.bandits)
+            end
+            if dis >= Config.CalloffBandits and spawnbandits then
+                --print('call off bandits')
+                calloffbandits = true
+            end
+        end
+    end
+end)
+
+CreateThread(function()
     npcs = {}
     horse = {}
     while true do
         Wait(1000)
-        if IsPedDeadOrDying(PlayerPedId(), true) and spawnbandits == true then
+        if IsPedDeadOrDying(PlayerPedId(), true) and spawnbandits then
             lib.notify({ title = ('looks like they got you'), duration = 5000, type = 'error' })
             Wait(5000)
             TriggerServerEvent('rsg-bandits:server:robplayer')
@@ -77,7 +106,7 @@ Citizen.CreateThread(function()
             spawnbandits = false
             break
         end
-        if calloffbandits == true then
+        if calloffbandits then
             for v,k in pairs(npcs) do
                 DeleteEntity(k)
             end
@@ -92,19 +121,9 @@ Citizen.CreateThread(function()
     end
 end)
 
--- cooldown timer
-function cooldownTimer()
-    cooldownSecondsRemaining = Config.Cooldown
-    Citizen.CreateThread(function()
-        while cooldownSecondsRemaining > 0 do
-            Wait(1000)
-            cooldownSecondsRemaining = cooldownSecondsRemaining - 1
-            print(cooldownSecondsRemaining)
-        end
-    end)
-end
-
+----------------------------------------------------------------------------
 -- delete bandits on resouce restart
+----------------------------------------------------------------------------
 AddEventHandler("onResourceStop",function(resourceName)
     for v,k in pairs(npcs) do
         DeleteEntity(k)
