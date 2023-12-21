a37b06cfe26fd2f6ad17a6c9b9198c75316c1795
RexshackGaming <rexshackuk@gmail.com>
2023-12-21 01:44:21 +0000

    version checker

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
 
-author 'RexShack#3041'
 description 'rsg-bandits'
+version '1.0.0'
 
 client_scripts {
     'client/client.lua'
